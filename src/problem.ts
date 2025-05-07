192043e942fe94c536d71ec23f3e767c292fdfa3
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-07 23:08:31 +0600

    Solve problem-1

--- src/problem.ts
+++ src/problem.ts
@@ -1,1 +1,28 @@
-console.log('test hoise')
+{
+  // Problem 1:
+
+  function formatString(input: string, toUpper: boolean = true): string {
+
+    return toUpper ? input.toUpperCase() : input.toLowerCase();
+
+  }
+
+  console.log(formatString("Hello")); // Output: "HELLO"
+  formatString("Hello", true); // Output: "HELLO"
+  console.log(formatString("Hello", false)); // Output: "hello"
+
+  console.log(formatString('Tanvir can do it', false))
+
+
+  ///probpem-2
+
+
+
+
+
+
+
+
+
+  
+}
